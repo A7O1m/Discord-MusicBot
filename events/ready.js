1916bbac90a6fca80b767a108abbef22befb8010
AlanDara <59530864+AlanDara@users.noreply.github.com>
2020-10-11 16:14:12 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("&help Music Bot High Quality", {
+  await client.user.setActivity("&help Music Bot BY Cg》DADA.LAVA ", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
